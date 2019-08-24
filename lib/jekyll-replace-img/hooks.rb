5b20fa102c9ea9b8ae724c44749e838b5a8a4665
Florian Klampfer <mail@qwtel.com>
2019-08-24 15:01:07 +0700

    Fix #1

--- lib/jekyll-replace-img/hooks.rb
+++ lib/jekyll-replace-img/hooks.rb
@@ -1,56 +1,58 @@
 # frozen_string_literal: true
 
-KEY_CONFIG = "replace_img"
+module JekyllReplaceImg
+  KEY_CONFIG = "replace_img"
 
-KEY_RE_IMG      = "re_img"
-KEY_RE_IGNORE   = "re_ignore"
-KEY_REPLACEMENT = "replacement"
+  KEY_RE_IMG      = "re_img"
+  KEY_RE_IGNORE   = "re_ignore"
+  KEY_REPLACEMENT = "replacement"
 
-RE_IMG      = "<img\\s*(?<attributes>.*?)\\s*/>"
-RE_IGNORE   = "data-ignore"
-REPLACEMENT = "
-  <hy-img %<attributes>s>
-    <noscript><img data-ignore %<attributes>s/></noscript>
-  </hy-img>"
+  RE_IMG      = "<img\\s*(?<attributes>.*?)\\s*/>"
+  RE_IGNORE   = "data-ignore"
+  REPLACEMENT = "
+    <hy-img %<attributes>s>
+      <noscript><img data-ignore %<attributes>s/></noscript>
+    </hy-img>"
 
-RE_DATAURL = %r!src\s*=\s*[""]\s*data:!ix.freeze
+  RE_DATAURL = %r!src\s*=\s*[""]\s*data:!ix.freeze
 
-re_img, re_ignore, replacement = nil
+  re_img, re_ignore, replacement = nil
 
-def get_config(config, key)
-  config[KEY_CONFIG] && config[KEY_CONFIG][key]
-end
+  def self.get_config(config, key)
+    config[KEY_CONFIG] && config[KEY_CONFIG][key]
+  end
 
-Jekyll::Hooks.register(:site, :after_init) do |site|
-  re_img = Regexp.new(
-    get_config(site.config, KEY_RE_IMG) || RE_IMG,
-    Regexp::EXTENDED | Regexp::IGNORECASE | Regexp::MULTILINE
-  )
+  Jekyll::Hooks.register(:site, :after_init) do |site|
+    re_img = Regexp.new(
+      JekyllReplaceImg.get_config(site.config, KEY_RE_IMG) || RE_IMG,
+      Regexp::EXTENDED | Regexp::IGNORECASE | Regexp::MULTILINE
+    )
 
-  re_ignore = Regexp.new(
-    get_config(site.config, KEY_RE_IGNORE) || RE_IGNORE,
-    Regexp::EXTENDED | Regexp::IGNORECASE
-  )
+    re_ignore = Regexp.new(
+      JekyllReplaceImg.get_config(site.config, KEY_RE_IGNORE) || RE_IGNORE,
+      Regexp::EXTENDED | Regexp::IGNORECASE
+    )
 
-  replacement = get_config(site.config, KEY_REPLACEMENT) || REPLACEMENT
-  ENV["JEKYLL_ENV"] == "production" && replacement.gsub!(%r!\n+!, "")
-end
+    replacement = JekyllReplaceImg.get_config(site.config, KEY_REPLACEMENT) || REPLACEMENT
+    ENV["JEKYLL_ENV"] == "production" && replacement.gsub!(%r!\n+!, "")
+  end
 
-Jekyll::Hooks.register([:pages, :documents], :post_render) do |page|
-  i = 0
-  page.output = page.output.gsub(re_img) do |match|
-    last_match = Regexp.last_match
+  Jekyll::Hooks.register([:pages, :documents], :post_render) do |page|
+    i = 0
+    page.output = page.output.gsub(re_img) do |match|
+      last_match = Regexp.last_match
 
-    subs = { :i => i }
-    last_match.names.each do |name|
-      subs[name.intern] = last_match[name]
-    end
+      subs = { :i => i }
+      last_match.names.each do |name|
+        subs[name.intern] = last_match[name]
+      end
 
-    if match.index(re_ignore).nil? && match.index(RE_DATAURL).nil?
-      i += 1
-      replacement % subs
-    else
-      match
+      if match.index(re_ignore).nil? && match.index(RE_DATAURL).nil?
+        i += 1
+        replacement % subs
+      else
+        match
+      end
     end
   end
 end
